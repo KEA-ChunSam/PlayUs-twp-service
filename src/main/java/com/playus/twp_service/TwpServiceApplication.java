5290b90667f7fa99df4c282c30a32940919dbe1e
han16935 <han1693516@gmail.com>
2025-04-09 13:22:16 +0900

    first commit

---
+++ src/main/java/com/playus/twp_service/TwpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.playus.twp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playus/twp_service/TwpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playus.twp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
