47fc65cbded9bb08d5c5cc5fbc380e201d929f8f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-15 17:04:18 +0900

    chore: 활성 프로필에 대한 테스트 구성 업데이트

--- src/test/java/com/playus/twp_service/TwpServiceApplicationTests.java
+++ src/test/java/com/playus/twp_service/TwpServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.playus.twp_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TwpServiceApplication.class, properties = "spring.profiles.active=test")
 class TwpServiceApplicationTests {
 
 	@Test
