daaa807348d7798a494ac1fed69b101798f2a3b4
han16935 <han1693516@gmail.com>
2025-04-18 16:58:47 +0900

    chore/TwpServiceApplication 내 주석 제거

--- src/main/java/com/playus/twp_service/TwpServiceApplication.java
+++ src/main/java/com/playus/twp_service/TwpServiceApplication.java
@@ -3,7 +3,6 @@ package com.playus.twp_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//
 @SpringBootApplication
 public class TwpServiceApplication {
 
