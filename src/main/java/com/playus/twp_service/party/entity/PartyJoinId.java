c9a24454fbdc04feaa59fe6cffd9a8d2d2083aba
han16935 <han1693516@gmail.com>
2025-04-23 16:27:09 +0900

    feat/PartyJoinId 추가

---
+++ src/main/java/com/playus/twp_service/party/entity/PartyJoinId.java
@@ -0,0 +1,18 @@
+package com.playus.twp_service.party.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class PartyJoinId implements Serializable {
+    private Long userId;
+    private Long partyId;
+}
