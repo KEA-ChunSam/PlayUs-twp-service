979323a65882207123ddda5f73ceeabf819651fb
han16935 <han1693516@gmail.com>
2025-04-30 14:19:27 +0900

    feat:PartyCreateResponse 작성

---
+++ src/main/java/com/playus/twp_service/party/dto/party_create/PartyCreateResponse.java
@@ -0,0 +1,17 @@
+package com.playus.twp_service.party.dto.party_create;
+
+import lombok.Builder;
+
+@Builder
+public record PartyCreateResponse (
+      Long partyId,
+      Boolean success
+) {
+
+    public static PartyCreateResponse of(Long partyId, Boolean success) {
+        return PartyCreateResponse.builder()
+                .partyId(partyId)
+                .success(success)
+                .build();
+    }
+}
