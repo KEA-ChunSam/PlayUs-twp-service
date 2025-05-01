a525c53c2cb895b5557c751f0c73e4a779ef347a
han16935 <han1693516@gmail.com>
2025-05-01 12:16:41 +0900

    test:ControllerTestSupport 작성

---
+++ src/test/java/com/playus/twp_service/ControllerTestSupport.java
@@ -0,0 +1,26 @@
+package com.playus.twp_service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.playus.twp_service.party.controller.PartyController;
+import com.playus.twp_service.party.service.PartyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ActiveProfiles("test")
+@WebFluxTest(controllers = {
+        PartyController.class
+})
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected WebTestClient webTestClient;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected PartyService partyService;
+}
