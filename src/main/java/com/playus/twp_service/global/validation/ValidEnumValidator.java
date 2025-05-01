5451c94a1faf9f7de56d90197b0bb03a3515196d
han16935 <han1693516@gmail.com>
2025-05-01 12:22:23 +0900

    feat:ValidEnumValidator 내 initialize에 Describable 적용 + isInvalidEnum() return 값 수정

--- src/main/java/com/playus/twp_service/global/validation/ValidEnumValidator.java
+++ src/main/java/com/playus/twp_service/global/validation/ValidEnumValidator.java
@@ -1,7 +1,9 @@
 package com.playus.twp_service.global.validation;
 
+import com.playus.twp_service.global.Describable;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
 import java.util.Set;
@@ -11,26 +13,24 @@ public class ValidEnumValidator implements ConstraintValidator<ValidEnum, String
 
     private Set<String> enumValues;
 
-    // enum의 값을 가져와 enumValues 에 저장
     @Override
     public void initialize(ValidEnum annotation) {
         enumValues = Arrays.stream(annotation.enumClass().getEnumConstants())
-                .map(Enum::name)
+                .map(Describable::getDescription)
                 .collect(Collectors.toSet());
     }
 
-    // enumValues 를 통한 실제 validation 로직 수행
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
         if (isInvalidEnum(value)) {
-            return true;
+            return false;
         }
 
-        // 필드의 값이 Enum 값들에 포함되어 있는지 확인
         return enumValues.contains(value);
     }
 
     private static boolean isInvalidEnum(String value) {
-        return value == null || value.isEmpty();
+        return StringUtils.isEmpty(value);
     }
 }
