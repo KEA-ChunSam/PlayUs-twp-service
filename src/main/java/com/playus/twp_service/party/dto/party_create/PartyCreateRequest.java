1eb0bf1473075eeb3d5aad0449022a2870cddbf3
han16935 <han1693516@gmail.com>
2025-05-02 20:43:36 +0900

    feat : PartyCreateRequest 에 최소/최대 인원 및 image url 추가 (관련 validation도 추가함)

--- src/main/java/com/playus/twp_service/party/dto/party_create/PartyCreateRequest.java
+++ src/main/java/com/playus/twp_service/party/dto/party_create/PartyCreateRequest.java
@@ -2,15 +2,21 @@ package com.playus.twp_service.party.dto.party_create;
 
 import com.playus.twp_service.global.validation.ValidEnum;
 import com.playus.twp_service.global.validation.ValidEnumList;
+import com.playus.twp_service.party.entity.Party;
 import com.playus.twp_service.party.enums.PartyJoinMethod;
 import com.playus.twp_service.party.enums.PartyAgeGroup;
 import com.playus.twp_service.party.enums.PartyGender;
+import com.playus.twp_service.party.validation.ValidMinimumMaximumParticipants;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.List;
 
+
 @Builder
+@ValidMinimumMaximumParticipants(message = "최소 참여 인원은 최대 참여 인원보다 클 수 없습니다!")
 public record PartyCreateRequest(
 
         @NotBlank(message = "직관팟 제목이 비어 있습니다!")
@@ -26,18 +32,36 @@ public record PartyCreateRequest(
                        invalidValueMessage = "잘못된 참여자 나이입니다!", overValueMessage = "참여자 나이는 최대 6개까지 가능합니다!")
         List<String> ageGroup,
 
+        @NotNull(message = "최소 참여 인원이 비어 있습니다!")
+        @Min(value = 1, message = "최소 참여 인원은 1명 이상이여야 합니다!")
+        Long minimumParticipants,
+
+        @NotNull(message = "최대 참여 인원이 비어 있습니다!")
+        @Min(value = 1, message = "최대 참여 인원은 1명 이상이여야 합니다!")
+        Long maximumParticipants,
+
+        @NotBlank(message = "사진 URL이 비어 있습니다!")
+        String thumbnailUrl,
+
         @NotBlank(message = "직관팟 소개 문구가 비어 있습니다!")
         String message
 
 ) {
 
-    public static PartyCreateRequest of(String title, String method, String gender, List<String> ageGroup, String message) {
+    public static PartyCreateRequest of(String title, String method, String gender, List<String> ageGroup,
+                                        Long minimumParticipants, Long maximumParticipants,
+                                        String thumbnailUrl, String message) {
+
         return PartyCreateRequest.builder()
                 .title(title)
                 .method(method)
                 .gender(gender)
                 .ageGroup(ageGroup)
+                .minimumParticipants(minimumParticipants)
+                .maximumParticipants(maximumParticipants)
+                .thumbnailUrl(thumbnailUrl)
                 .message(message)
                 .build();
     }
+
 }
