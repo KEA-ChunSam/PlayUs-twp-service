db61f93dadfbabcf572b08bd9ccc677220e8a04d
han16935 <han1693516@gmail.com>
2025-05-03 20:15:27 +0900

    test : Spring Security 추가로 인한 controller test 전용 filterChain 작성 (모든 URL에 "USER" 권한 필요하도록)

--- src/test/java/com/playus/twp_service/ControllerTestSupport.java
+++ src/test/java/com/playus/twp_service/ControllerTestSupport.java
@@ -4,14 +4,22 @@ import com.playus.twp_service.party.controller.PartyController;
 import com.playus.twp_service.party.service.PartyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
 
 @ActiveProfiles("test")
 @WebFluxTest(controllers = {
         PartyController.class
 })
+@Import({ControllerTestSupport.TestSecurityConfig.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -19,4 +27,21 @@ public abstract class ControllerTestSupport {
 
     @MockitoBean
     protected PartyService partyService;
+
+    @TestConfiguration
+    @EnableWebFluxSecurity
+    static class TestSecurityConfig {
+
+        @Bean
+        public SecurityWebFilterChain testSecurityFilterChain(ServerHttpSecurity http) {
+            return http
+                    .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                    .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                    .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                    .authorizeExchange(exchanges -> exchanges
+                            .anyExchange().hasAuthority("USER")
+                    )
+                    .build();
+        }
+    }
 }
