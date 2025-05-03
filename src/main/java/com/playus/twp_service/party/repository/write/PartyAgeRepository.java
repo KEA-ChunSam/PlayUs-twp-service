619b469f272681dbac0ad9faa4c62505212f3c96
han16935 <han1693516@gmail.com>
2025-05-03 20:21:27 +0900

    feat : PartyAge, PartyJoinRepository 작성

---
+++ src/main/java/com/playus/twp_service/party/repository/write/PartyAgeRepository.java
@@ -0,0 +1,7 @@
+package com.playus.twp_service.party.repository.write;
+
+import com.playus.twp_service.party.entity.PartyAge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyAgeRepository extends JpaRepository<PartyAge, Long> {
+}
---
+++ src/main/java/com/playus/twp_service/party/repository/write/PartyJoinRepository.java
@@ -0,0 +1,7 @@
+package com.playus.twp_service.party.repository.write;
+
+import com.playus.twp_service.party.entity.PartyJoin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyJoinRepository extends JpaRepository<PartyJoin, Long> {
+}
