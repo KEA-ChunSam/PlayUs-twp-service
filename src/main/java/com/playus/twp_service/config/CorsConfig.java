a7a8ca16db27a35d993f9acbdac5692d0d61c4e0
han16935 <han1693516@gmail.com>
2025-05-03 20:18:45 +0900

    feat : CorsConfig 추가 (따로 SecurityConfig 추가 없이 corsFilter bean 등록으로 적용)

---
+++ src/main/java/com/playus/twp_service/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.playus.twp_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.WebFilter;
+
+import java.util.Collections;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public WebFilter corsFilter(CorsConfigurationSource corsConfigurationSource) {
+        return new org.springframework.web.cors.reactive.CorsWebFilter(corsConfigurationSource);
+    }
+}
