e4078d4ff7547668c9706c543662c1f89bc9b50d
han16935 <han1693516@gmail.com>
2025-05-05 09:41:42 +0900

    refactor : Webflux 사용 취소로 인한 ControllerTestSupport 리팩토링

--- src/test/java/com/playus/twp_service/ControllerTestSupport.java
+++ src/test/java/com/playus/twp_service/ControllerTestSupport.java
@@ -1,47 +1,59 @@
 package com.playus.twp_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.playus.twp_service.party.controller.PartyController;
 import com.playus.twp_service.party.service.PartyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 
 @ActiveProfiles("test")
-@WebFluxTest(controllers = {
+@WebMvcTest(controllers = {
         PartyController.class
 })
 @Import({ControllerTestSupport.TestSecurityConfig.class})
 public abstract class ControllerTestSupport {
 
+
     @Autowired
-    protected WebTestClient webTestClient;
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
 
     @MockitoBean
     protected PartyService partyService;
 
     @TestConfiguration
-    @EnableWebFluxSecurity
     static class TestSecurityConfig {
 
         @Bean
-        public SecurityWebFilterChain testSecurityFilterChain(ServerHttpSecurity http) {
-            return http
-                    .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                    .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                    .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                    .authorizeExchange(exchanges -> exchanges
-                            .anyExchange().hasAuthority("USER")
-                    )
-                    .build();
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.csrf(AbstractHttpConfigurer::disable);
+            http.formLogin(AbstractHttpConfigurer::disable);
+            http.httpBasic(AbstractHttpConfigurer::disable);
+            http.authorizeHttpRequests(auth -> auth
+                    .anyRequest().hasAuthority("USER")
+            );
+
+            // 세션 관리: Stateless
+            http.sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+
+            return http.build();
+
         }
     }
 }
