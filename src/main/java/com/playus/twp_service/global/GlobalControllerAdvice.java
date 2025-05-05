187e49732c2a6792a11ff5dd08aa6c6d1d782663
han16935 <han1693516@gmail.com>
2025-05-05 14:38:07 +0900

    feat : controllerAdvice 에 invalidDescriptionException 등로

--- src/main/java/com/playus/twp_service/global/GlobalControllerAdvice.java
+++ src/main/java/com/playus/twp_service/global/GlobalControllerAdvice.java
@@ -1,6 +1,9 @@
 package com.playus.twp_service.global;
 
 import com.playus.twp_service.domain.party.controller.PartyController;
+import com.playus.twp_service.domain.party.exception.enums.PartyAgeGroupExceptionGroup;
+import com.playus.twp_service.domain.party.exception.enums.PartyGenderExceptionGroup;
+import com.playus.twp_service.domain.party.exception.enums.PartyJoinMethodExceptionGroup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,18 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(BindException.class)
     public ResponseEntity<String> bindExceptionHandler(BindException e) {
         String errorMessage = e.getAllErrors().get(0).getDefaultMessage();
+        log.warn("Validation Error: {}", errorMessage);
+        return ResponseEntity.badRequest().body(errorMessage);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({
+            PartyGenderExceptionGroup.InvalidDescriptionException.class,
+            PartyJoinMethodExceptionGroup.InvalidDescriptionException.class,
+            PartyAgeGroupExceptionGroup.InvalidDescriptionException.class
+    })
+    public ResponseEntity<String> invalidDescriptionExceptionHandler(Exception e) {
+        String errorMessage = e.getMessage();
         log.warn("Validation Error: {}", errorMessage);
         return ResponseEntity.badRequest().body(errorMessage);
     }
