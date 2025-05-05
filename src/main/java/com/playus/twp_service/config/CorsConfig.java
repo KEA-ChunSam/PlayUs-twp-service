3a24a937ef7fa9c5471d5741aabf3e27923981ec
han16935 <han1693516@gmail.com>
2025-05-05 09:41:56 +0900

    refactor : Webflux 사용 취소로 인한 CorsConfig 리팩토링

--- src/main/java/com/playus/twp_service/config/CorsConfig.java
+++ src/main/java/com/playus/twp_service/config/CorsConfig.java
@@ -4,33 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.server.WebFilter;
-
-import java.util.Collections;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    @Bean("corsConfigurationSource")
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-
-    @Bean
-    public WebFilter corsFilter(CorsConfigurationSource corsConfigurationSource) {
-        return new org.springframework.web.cors.reactive.CorsWebFilter(corsConfigurationSource);
     }
 }
