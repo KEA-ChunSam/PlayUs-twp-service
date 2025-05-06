66c71e0894018e074b1f56c732f292476ca2806c
han16935 <han1693516@gmail.com>
2025-05-06 15:35:02 +0900

    feat : PartyThumbnailUrl entity 작성 및 그에 따른 Repository 작성

---
+++ src/main/java/com/playus/twp_service/domain/party/repository/write/PartyThumbnailUrlRepository.java
@@ -0,0 +1,8 @@
+package com.playus.twp_service.domain.party.repository.write;
+
+import com.playus.twp_service.domain.party.entity.PartyThumbnailUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyThumbnailUrlRepository extends JpaRepository<PartyThumbnailUrl, Long> {
+
+}
