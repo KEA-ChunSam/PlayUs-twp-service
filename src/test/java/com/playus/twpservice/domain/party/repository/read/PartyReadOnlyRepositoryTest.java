f9fc198b25faa3be4293f7d736d7ad0452cc3365
han16935 <han1693516@gmail.com>
2025-05-12 22:09:15 +0900

    test : findPartySummariesByMatchId() 테스트 작성

--- src/test/java/com/playus/twpservice/domain/party/repository/read/PartyReadOnlyRepositoryTest.java
+++ src/test/java/com/playus/twpservice/domain/party/repository/read/PartyReadOnlyRepositoryTest.java
@@ -1,8 +1,84 @@
 package com.playus.twpservice.domain.party.repository.read;
 
 import com.playus.twpservice.IntegrationTestSupport;
+import com.playus.twpservice.domain.party.document.PartyAgeDocument;
+import com.playus.twpservice.domain.party.document.PartyDocument;
+import com.playus.twpservice.domain.party.document.PartyJoinDocument;
+import com.playus.twpservice.domain.party.document.PartyThumbnailUrlDocument;
+import com.playus.twpservice.domain.party.enums.PartyGender;
+import com.playus.twpservice.domain.party.enums.PartyJoinMethod;
+import com.playus.twpservice.domain.party.enums.PartyJoinRequestStatus;
+import com.playus.twpservice.domain.party.vo.PartySummary;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 class PartyReadOnlyRepositoryTest extends IntegrationTestSupport {
 
+    @Autowired
+    PartyReadOnlyRepository partyReadOnlyRepository;
 
+    @Autowired
+    PartyAgeReadOnlyRepository partyAgeReadOnlyRepository;
+
+    @Autowired
+    PartyThumbnailUrlReadOnlyRepository partyThumbnailUrlReadOnlyRepository;
+
+    @Autowired
+    PartyJoinReadOnlyRepository partyJoinReadOnlyRepository;
+
+
+    @AfterEach
+    void tearDown() {
+        partyReadOnlyRepository.deleteAll();
+        partyAgeReadOnlyRepository.deleteAll();
+        partyThumbnailUrlReadOnlyRepository.deleteAll();
+        partyJoinReadOnlyRepository.deleteAll();
+    }
+
+    @DisplayName("직관팟 정보를 가져올 수 있다.")
+    @Test
+    void findPartySummaries() {
+        // given
+        Long matchId = 1L;
+
+        PartyDocument p1 = PartyDocument.createForOnlyTest(1L, "title1", "text1", 1L, 10L,
+                "http://thumbnail", PartyGender.MALE, PartyJoinMethod.FIRST_COME, matchId, "chatRoomId");
+        PartyDocument p2 = PartyDocument.createForOnlyTest(2L, "title2", "text2", 1L, 10L,
+                "http://thumbnail", PartyGender.FEMALE, PartyJoinMethod.RESERVATION, matchId, "chatRoom2Id");
+        PartyDocument p3 = PartyDocument.createForOnlyTest(3L, "title3", "text3", 1L, 10L,
+                "http://thumbnail", PartyGender.NO_MATTER, PartyJoinMethod.RESERVATION, 2L, "chatRoom3Id");
+        List<PartyDocument> partyDocuments = partyReadOnlyRepository.saveAll(List.of(p1, p2, p3));
+
+        PartyAgeDocument pa1 = PartyAgeDocument.createForOnlyTest(1L, partyDocuments.get(0), 10);
+        PartyAgeDocument pa2 = PartyAgeDocument.createForOnlyTest(2L, partyDocuments.get(1), 20);
+        partyAgeReadOnlyRepository.saveAll(List.of(pa1, pa2));
+
+        PartyThumbnailUrlDocument ptu1 = PartyThumbnailUrlDocument.createForOnlyTest(1L, partyDocuments.get(0), "thumbnailUrl1");
+        PartyThumbnailUrlDocument ptu2 = PartyThumbnailUrlDocument.createForOnlyTest(2L, partyDocuments.get(0), "thumbnailUrl2");
+        partyThumbnailUrlReadOnlyRepository.saveAll(List.of(ptu1, ptu2));
+
+        PartyJoinDocument pj1 = PartyJoinDocument.createForOnlyTest(1L, 1L, partyDocuments.get(0), PartyJoinRequestStatus.ACCEPT, null);
+        PartyJoinDocument pj2 = PartyJoinDocument.createForOnlyTest(2L, 2L, partyDocuments.get(0), PartyJoinRequestStatus.WAIT, "가입 원합니다!");
+        PartyJoinDocument pj3 = PartyJoinDocument.createForOnlyTest(3L, 3L, partyDocuments.get(1), PartyJoinRequestStatus.ACCEPT, null);
+        partyJoinReadOnlyRepository.saveAll(List.of(pj1, pj2, pj3));
+
+        // when
+        List<PartySummary> result = partyReadOnlyRepository.findPartySummariesByMatchId(matchId);
+
+        // then
+        assertThat(result).hasSize(2)
+                .extracting("partyId", "title", "partyJoinMethod", "partyGender", "ages",
+                        "currentParticipantsCount", "maximumParticipants", "thumbnailUrls")
+                .containsExactlyInAnyOrder(
+                        tuple(1L, "title1", PartyJoinMethod.FIRST_COME, PartyGender.MALE, List.of(10), 2L, 10L, List.of("thumbnailUrl1", "thumbnailUrl2")),
+                        tuple(2L, "title2", PartyJoinMethod.RESERVATION, PartyGender.FEMALE, List.of(20), 1L, 10L, List.of())
+                );
+    }
 }
