10aa7b9dc1c513bc8952d11afc184b0102869fa2
han16935 <han1693516@gmail.com>
2025-05-12 22:07:45 +0900

    feat : 직관팟 정보 가져올 PartySummary vo 작성

---
+++ src/main/java/com/playus/twpservice/domain/party/vo/PartySummary.java
@@ -0,0 +1,23 @@
+package com.playus.twpservice.domain.party.vo;
+
+import com.playus.twpservice.domain.party.enums.PartyGender;
+import com.playus.twpservice.domain.party.enums.PartyJoinMethod;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartySummary {
+    private Long partyId;
+    private String title;
+    private PartyJoinMethod partyJoinMethod;
+    private PartyGender partyGender;
+    private List<Integer> ages;
+    private Long currentParticipantsCount;
+    private Long maximumParticipants;
+    private List<String> thumbnailUrls;
+}
