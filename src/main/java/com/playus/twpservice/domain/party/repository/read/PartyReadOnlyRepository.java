d1266522e5869670b6af51560f57bac5c2a89a7c
han16935 <han1693516@gmail.com>
2025-05-12 22:09:02 +0900

    feat : findPartySummariesByMatchId() 위한 custom repo 작성

--- src/main/java/com/playus/twpservice/domain/party/repository/read/PartyReadOnlyRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/read/PartyReadOnlyRepository.java
@@ -3,5 +3,6 @@ package com.playus.twpservice.domain.party.repository.read;
 import com.playus.twpservice.domain.party.document.PartyDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PartyReadOnlyRepository extends MongoRepository<PartyDocument, Long> {
+public interface PartyReadOnlyRepository extends MongoRepository<PartyDocument, Long>, PartyReadOnlyRepositoryCustom {
+
 }
