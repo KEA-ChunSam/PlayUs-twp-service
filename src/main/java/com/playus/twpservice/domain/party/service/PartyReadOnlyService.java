d0df22dbe0e23bcfc6f10c38a6b7ddcc86d7082a
han16935 <han1693516@gmail.com>
2025-05-12 22:08:04 +0900

    feat : PartyReadOnlyService 초안 작성

--- src/main/java/com/playus/twpservice/domain/party/service/PartyReadOnlyService.java
+++ src/main/java/com/playus/twpservice/domain/party/service/PartyReadOnlyService.java
@@ -1,6 +1,10 @@
 package com.playus.twpservice.domain.party.service;
 
 import com.playus.twpservice.domain.party.dto.partybymatch.PartiesByMatchResponse;
+import com.playus.twpservice.domain.party.repository.read.PartyAgeReadOnlyRepository;
+import com.playus.twpservice.domain.party.repository.read.PartyJoinReadOnlyRepository;
+import com.playus.twpservice.domain.party.repository.read.PartyReadOnlyRepository;
+import com.playus.twpservice.domain.party.repository.read.PartyThumbnailUrlReadOnlyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +14,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class PartyReadOnlyService {
 
+    private final PartyReadOnlyRepository partyRepository;
+    private final PartyAgeReadOnlyRepository partyAgeRepository;
+    private final PartyJoinReadOnlyRepository partyJoinRepository;
+    private final PartyThumbnailUrlReadOnlyRepository partyThumbnailUrlRepository;
+
     public PartiesByMatchResponse getPartiesBy(Long matchId, Long userId) {
+
+        // 1. 직관팟 데이터 가져오기 (title, method, ages, gender, currentParticipantsCount,
+        //                         maximumParticipantsCount, thumbnailurls)
+
+
+
+        // 2. user 데이터 가져오기 (authorName, authorGender, userThumbnailUrls)
+
+        // 3. match 데이터 가져오기 (matchDate)
+
         return null;
     }
 }
