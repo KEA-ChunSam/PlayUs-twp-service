de0d804c60f5f632ec17c66f1a07069cb95e9eb3
han16935 <han1693516@gmail.com>
2025-05-13 20:16:13 +0900

    refactor : UserFeignFallback 내 @Component 추가 및 fallback 시 response 수정

--- src/main/java/com/playus/twpservice/domain/party/feign/fallback/UserFeignFallback.java
+++ src/main/java/com/playus/twpservice/domain/party/feign/fallback/UserFeignFallback.java
@@ -1,19 +1,22 @@
 package com.playus.twpservice.domain.party.feign.fallback;
 
 import com.playus.twpservice.domain.party.feign.client.UserFeignClient;
+import com.playus.twpservice.domain.party.feign.response.PartyUserThumbnailUrlListResponse;
 import com.playus.twpservice.domain.party.feign.response.PartyWriterInfoFeignResponse;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class UserFeignFallback implements UserFeignClient {
 
     @Override
     public List<PartyWriterInfoFeignResponse> getWriterInfo(List<Long> writerIdList) {
-        return List.of();
+        return List.of(PartyWriterInfoFeignResponse.withServiceUnavailable());
     }
 
     @Override
-    public List<String> getPartyUserThumbnailUrls(List<Long> userIdList) {
-        return List.of();
+    public PartyUserThumbnailUrlListResponse getPartyUserThumbnailUrls(List<Long> userIdList) {
+        return PartyUserThumbnailUrlListResponse.withServiceUnavailable();
     }
 }
