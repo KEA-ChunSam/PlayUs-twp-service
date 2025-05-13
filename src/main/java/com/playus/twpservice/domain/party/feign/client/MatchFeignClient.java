263112947068153c50c69f359999e6c1c7b70bad
han16935 <han1693516@gmail.com>
2025-05-13 20:13:17 +0900

    refactor : MatchFeignClient name 및 경로 명시

--- src/main/java/com/playus/twpservice/domain/party/feign/client/MatchFeignClient.java
+++ src/main/java/com/playus/twpservice/domain/party/feign/client/MatchFeignClient.java
@@ -1,16 +1,17 @@
 package com.playus.twpservice.domain.party.feign.client;
 
+import com.playus.twpservice.domain.party.feign.fallback.MatchFeignFallback;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-// match-service, /match/api 예상 중
-@FeignClient(name = "${feign.match.domain}", url = "${feign.match.url}", fallback = MatchFeignClient.class)
+@FeignClient(name = "matchFeignClient", url = "${feign.match.url}", fallback = MatchFeignFallback.class)
 @CircuitBreaker(name = "circuit")
 public interface MatchFeignClient {
 
-    List<LocalDateTime> getMatchDate(@RequestBody List<Long> matchIdList);
+    @GetMapping("/api/match")
+    LocalDateTime getMatchDate(@RequestParam Long matchId);
 }
