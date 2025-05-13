1a9379949b9c6fa58c7e6cabc57cf5385043e6d1
han16935 <han1693516@gmail.com>
2025-05-13 16:21:51 +0900

    feature : @EnableFeignClients 추가

--- src/main/java/com/playus/twpservice/TwpServiceApplication.java
+++ src/main/java/com/playus/twpservice/TwpServiceApplication.java
@@ -2,7 +2,9 @@ package com.playus.twpservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class TwpServiceApplication {
 
