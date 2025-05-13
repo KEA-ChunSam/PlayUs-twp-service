464d62a8aaecb2b6be453a21882a8d32cea5f736
han16935 <han1693516@gmail.com>
2025-05-13 20:13:52 +0900

    refactor : MatchFeignFallback 내 @Component 작성 및 return 결과 변경

--- src/main/java/com/playus/twpservice/domain/party/feign/fallback/MatchFeignFallback.java
+++ src/main/java/com/playus/twpservice/domain/party/feign/fallback/MatchFeignFallback.java
@@ -1,15 +1,15 @@
 package com.playus.twpservice.domain.party.feign.fallback;
 
 import com.playus.twpservice.domain.party.feign.client.MatchFeignClient;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
+@Component
 public class MatchFeignFallback implements MatchFeignClient {
 
     @Override
-    public List<LocalDateTime> getMatchDate(List<Long> matchIdList) {
-        return List.of();
+    public LocalDateTime getMatchDate(Long matchId) {
+        return LocalDateTime.of(2023, 10, 1, 0, 0);
     }
-
 }
