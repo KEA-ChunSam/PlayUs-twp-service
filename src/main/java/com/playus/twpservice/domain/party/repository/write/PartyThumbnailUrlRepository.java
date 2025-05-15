963b5bd33f13abfc822cf8ba89d061d88d2e965b
han16935 <han1693516@gmail.com>
2025-05-15 21:48:32 +0900

    feat : PartyAge, ThumbnailUrl 에 deleteByPartyId 추가

--- src/main/java/com/playus/twpservice/domain/party/repository/write/PartyThumbnailUrlRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/write/PartyThumbnailUrlRepository.java
@@ -4,5 +4,5 @@ import com.playus.twpservice.domain.party.entity.PartyThumbnailUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartyThumbnailUrlRepository extends JpaRepository<PartyThumbnailUrl, Long> {
-
+    void deleteByPartyId(Long partyId);
 }
