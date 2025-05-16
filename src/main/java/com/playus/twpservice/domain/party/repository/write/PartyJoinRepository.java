c9339af386a4dcb418c3421d627842a3b4cc3172
han16935 <han1693516@gmail.com>
2025-05-17 00:23:08 +0900

    feat : PartyJoinRepository 에 deleteByPartId() 추가

--- src/main/java/com/playus/twpservice/domain/party/repository/write/PartyJoinRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/write/PartyJoinRepository.java
@@ -4,4 +4,5 @@ import com.playus.twpservice.domain.party.entity.PartyJoin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartyJoinRepository extends JpaRepository<PartyJoin, Long> {
+    void deleteByPartyId(Long partyId);
 }
