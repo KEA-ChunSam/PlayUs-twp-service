c131aee56dcbd8ac518225ba687e05c6dd370902
han16935 <han1693516@gmail.com>
2025-05-17 00:25:28 +0900

    feat : 직관팟 삭제 service 작성

--- src/main/java/com/playus/twpservice/domain/party/service/PartyService.java
+++ src/main/java/com/playus/twpservice/domain/party/service/PartyService.java
@@ -2,9 +2,11 @@ package com.playus.twpservice.domain.party.service;
 
 import com.playus.twpservice.domain.chat.entity.ChatPart;
 import com.playus.twpservice.domain.chat.entity.ChatRoom;
+import com.playus.twpservice.domain.chat.repository.ChatMessageRepository;
 import com.playus.twpservice.domain.chat.repository.ChatPartRepository;
 import com.playus.twpservice.domain.chat.repository.ChatRoomRepository;
 import com.playus.twpservice.domain.party.assertion.PartyAssert;
+import com.playus.twpservice.domain.party.document.PartyDocument;
 import com.playus.twpservice.domain.party.dto.partycreate.PartyCreateRequest;
 import com.playus.twpservice.domain.party.dto.partycreate.PartyCreateResponse;
 import com.playus.twpservice.domain.party.dto.partydelete.PartyDeleteResponse;
@@ -17,7 +19,9 @@ import com.playus.twpservice.domain.party.entity.Party;
 import com.playus.twpservice.domain.party.entity.PartyAge;
 import com.playus.twpservice.domain.party.entity.PartyThumbnailUrl;
 import com.playus.twpservice.domain.party.enums.PartyAgeGroup;
+import com.playus.twpservice.domain.party.repository.read.PartyReadOnlyRepository;
 import com.playus.twpservice.domain.party.repository.write.PartyAgeRepository;
+import com.playus.twpservice.domain.party.repository.write.PartyJoinRepository;
 import com.playus.twpservice.domain.party.repository.write.PartyRepository;
 import com.playus.twpservice.domain.party.repository.write.PartyThumbnailUrlRepository;
 import com.playus.twpservice.global.s3.S3PresignedUrlGenerator;
@@ -39,10 +43,14 @@ public class PartyService {
     private final PartyAgeRepository partyAgeRepository;
     private final PartyThumbnailUrlRepository partyThumbnailUrlRepository;
 
+
     private final ChatRoomRepository chatRoomRepository;
     private final ChatPartRepository chatPartRepository;
+    private final ChatMessageRepository chatMessageRepository;
 
     private final S3PresignedUrlGenerator s3PresignedUrlGenerator;
+    private final PartyReadOnlyRepository partyReadOnlyRepository;
+    private final PartyJoinRepository partyJoinRepository;
 
     public PartyCreateResponse createParty(Long userId, PartyCreateRequest request) {
         ChatRoom chatRoom = initializeChatRoomAsWriter(userId, request);
@@ -75,8 +83,30 @@ public class PartyService {
     }
 
     public PartyDeleteResponse deleteParty(Long userId, Long partyId, Long writerId) {
-//        PartyAssert.isLoginUserWriter(userId);
-        return null;
+
+        PartyAssert.isLoginUserWriter(userId, writerId);
+
+        PartyDocument partyDocument = partyReadOnlyRepository.findById(partyId)
+                .orElseThrow(() -> new NotFoundException("잘못된 직관팟 번호입니다!"));
+
+        partyThumbnailUrlRepository.deleteByPartyId(partyId);
+        partyAgeRepository.deleteByPartyId(partyId);
+        partyJoinRepository.deleteByPartyId(partyId);
+        partyRepository.deleteById(partyId);
+
+        String chatRoomId = partyDocument.getChatRoomId();
+
+        List<ChatPart> chatParts = chatPartRepository.findByChatRoomId(chatRoomId);
+        List<String> chatPartIds = chatParts.stream()
+                .map(ChatPart::getId)
+                .toList();
+
+
+        chatMessageRepository.deleteAllByChatPartIds(chatPartIds);
+        chatPartRepository.deleteByChatRoomId(chatRoomId);
+        chatRoomRepository.deleteById(chatRoomId);
+
+        return PartyDeleteResponse.of(partyId);
     }
 
     public PresignedUrlForSaveImageResponse generatePresignedUrlForSaveImage(PresignedUrlForSaveImageRequest request) {
@@ -105,7 +135,7 @@ public class PartyService {
 
     private ChatRoom initializeChatRoomAsWriter(Long userId, PartyCreateRequest request) {
         ChatRoom chatRoom = chatRoomRepository.save(ChatRoom.create(request.title()));
-        chatPartRepository.save(ChatPart.create(userId, chatRoom));
+        chatPartRepository.save(ChatPart.create(userId, chatRoom.getId()));
         return chatRoom;
     }
 
