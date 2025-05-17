b53f98b302c908bc41c7b114cdfc60ddadd8e07f
han16935 <han1693516@gmail.com>
2025-05-17 09:20:39 +0900

    performance : PartyAge, PartyThumbnailurlRepo 내 deleteByPartyId() 에 query 명시함으로써 쿼리 실행 횟수 최적화 (service 단 테스트에서 쿼리 확인)

--- src/main/java/com/playus/twpservice/domain/party/repository/write/PartyAgeRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/write/PartyAgeRepository.java
@@ -2,7 +2,12 @@ package com.playus.twpservice.domain.party.repository.write;
 
 import com.playus.twpservice.domain.party.entity.PartyAge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PartyAgeRepository extends JpaRepository<PartyAge, Long> {
+
+    @Modifying
+    @Query("UPDATE PartyAge p SET p.deletedAt = CURRENT_TIMESTAMP WHERE p.party.id = :partyId")
     void deleteByPartyId(Long partyId);
 }
