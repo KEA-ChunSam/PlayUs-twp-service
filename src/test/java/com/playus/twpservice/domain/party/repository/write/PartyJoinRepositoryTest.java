78b489d283d46bbe05643d7c68449c09c7c6aa58
han16935 <han1693516@gmail.com>
2025-05-17 09:10:37 +0900

    performance : deleteByPartyId() 에 query 명시함으로써 쿼리 실행 횟수 최적화 + 테스트 완료

---
+++ src/test/java/com/playus/twpservice/domain/party/repository/write/PartyJoinRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.playus.twpservice.domain.party.repository.write;
+
+import com.playus.twpservice.IntegrationTestSupport;
+import com.playus.twpservice.domain.party.entity.Party;
+import com.playus.twpservice.domain.party.entity.PartyJoin;
+import com.playus.twpservice.domain.party.enums.PartyGender;
+import com.playus.twpservice.domain.party.enums.PartyJoinMethod;
+import com.playus.twpservice.domain.party.enums.PartyJoinRequestStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+class PartyJoinRepositoryTest extends IntegrationTestSupport {
+
+    @Autowired
+    PartyRepository partyRepository;
+
+    @Autowired
+    PartyJoinRepository partyJoinRepository;
+
+    @AfterEach
+    void tearDown() {
+        partyJoinRepository.deleteAll();
+        partyRepository.deleteAll();
+    }
+
+    @DisplayName("직관팟 ID에 따라 PartyJoin을 삭제할 수 있다.")
+    @Test
+    void deleteByPartyId() {
+        // given
+        Long writerId = 1L;
+        Long matchId = 1L;
+        Party party = partyRepository.save(Party.create("title2", "16일 경기 같이 보실 분~", 1L, 15L,
+                PartyGender.FEMALE, PartyJoinMethod.RESERVATION, writerId, matchId).assignChatRoom("TEST-CHATROOM"));
+
+        Party otherParty = partyRepository.save(Party.create("title2", "16일 경기 같이 보실 분~", 1L, 15L,
+                PartyGender.FEMALE, PartyJoinMethod.RESERVATION, writerId, matchId).assignChatRoom("TEST-CHATROOM"));
+
+        partyJoinRepository.saveAll(List.of(PartyJoin.create(1L, party, PartyJoinRequestStatus.WAIT, null),
+                PartyJoin.create(1L, party, PartyJoinRequestStatus.WAIT, null),
+                PartyJoin.create(2L, otherParty, PartyJoinRequestStatus.ACCEPT, "잘 부탁드려요!")));
+
+        // when
+        partyJoinRepository.deleteByPartyId(party.getId());
+
+        // then
+        assertThat(partyJoinRepository.count()).isEqualTo(1);
+    }
+}
