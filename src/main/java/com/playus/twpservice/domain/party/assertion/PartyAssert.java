1e5086a95ef0878f8a91c549831821b7fb8de274
han16935 <han1693516@gmail.com>
2025-05-18 18:52:57 +0900

    feat : 현재 참여 가능한지 파악하는 isAppliableParty() 작성

--- src/main/java/com/playus/twpservice/domain/party/assertion/PartyAssert.java
+++ src/main/java/com/playus/twpservice/domain/party/assertion/PartyAssert.java
@@ -1,5 +1,6 @@
 package com.playus.twpservice.domain.party.assertion;
 
+import com.playus.twpservice.domain.party.entity.Party;
 import org.springframework.util.Assert;
 
 import java.util.Objects;
@@ -11,6 +12,12 @@ public class PartyAssert extends Assert {
     public static void isLoginUserWriter(Long userId, Long writerId) {
         if (!Objects.equals(userId, writerId)) {
             throw new NotPartyWriterException("직관팟 작성자가 아니면 수정할 수 없습니다!");
+        }
+    }
+
+    public static void isAppliableParty(Party party) {
+        if (party.getCurrentParticipants() >= party.getMaximumParticipants()) {
+            throw new ExceedPartyParticipantsException("직관팟 정원이 초과되었습니다!");
         }
     }
 }
