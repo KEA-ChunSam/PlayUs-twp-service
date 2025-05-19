7689f85be68a94784a9e0d1e606bf0c181aef31c
han16935 <han1693516@gmail.com>
2025-05-19 17:34:03 +0900

    fix : TwpServiceApplication에 @SecurityScheme 추가

--- src/main/java/com/playus/twpservice/TwpServiceApplication.java
+++ src/main/java/com/playus/twpservice/TwpServiceApplication.java
@@ -1,10 +1,19 @@
 package com.playus.twpservice;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SecurityScheme(
+		name      = "AccessCookie",
+		type      = SecuritySchemeType.APIKEY,
+		in        = SecuritySchemeIn.COOKIE,
+		paramName = "Access"
+)
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
