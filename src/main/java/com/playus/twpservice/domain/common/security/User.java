7fdc5774e1f5498064b43429c527857da0ee5bc1
han16935 <han1693516@gmail.com>
2025-05-19 23:46:20 +0900

    fix : User 에서 @Table 제거

--- src/main/java/com/playus/twpservice/domain/common/security/User.java
+++ src/main/java/com/playus/twpservice/domain/common/security/User.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "users")
 public class User {
 
     public static final float DEFAULT_SCORE = 0.3f;
@@ -39,33 +38,4 @@ public class User {
     private LocalDateTime blockOff;
 
     private LocalDateTime createdAt;
-
-    @Builder
-    private User(String nickname,String phoneNumber, LocalDate birth, Gender gender, Role role, AuthProvider authProvider, boolean activated, LocalDateTime blockOff, String thumbnailURL, Float userScore) {
-        this.nickname = nickname;
-        this.phoneNumber = phoneNumber;
-        this.birth = birth;
-        this.gender = gender;
-        this.role = role;
-        this.authProvider = authProvider;
-        this.activated = activated;
-        this.blockOff = blockOff;
-        this.thumbnailURL = thumbnailURL;
-        this.userScore = userScore;
-    }
-
-    public static User create(String nickname,String phoneNumber, LocalDate birth, Gender gender, Role role, AuthProvider authProvider, String thumbnailURL) {
-        return User.builder()
-                .nickname(nickname)
-                .phoneNumber(phoneNumber)
-                .birth(birth)
-                .gender(gender)
-                .role(role)
-                .authProvider(authProvider)
-                .activated(true)
-                .blockOff(null)
-                .thumbnailURL(thumbnailURL)
-                .userScore(DEFAULT_SCORE)
-                .build();
-    }
 }
