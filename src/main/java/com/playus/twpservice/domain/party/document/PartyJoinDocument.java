db6ab72c1deeffed0d640f415580602b5fd5bb64
han16935 <han1693516@gmail.com>
2025-05-19 23:45:55 +0900

    refactor : PartyJoinDocument에서 @DocumentReference 제거

--- src/main/java/com/playus/twpservice/domain/party/document/PartyJoinDocument.java
+++ src/main/java/com/playus/twpservice/domain/party/document/PartyJoinDocument.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
