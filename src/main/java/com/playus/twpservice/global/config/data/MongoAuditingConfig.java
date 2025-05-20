979a745fbea0a59c09af693ea3ce468d522b02bc
han16935 <han1693516@gmail.com>
2025-05-20 09:52:56 +0900

    fix : 기존 AuditingConfig 에서 MongoAuditingConfig 분리

---
+++ src/main/java/com/playus/twpservice/global/config/data/MongoAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.playus.twpservice.global.config.data;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public abstract class MongoAuditingConfig {
+}
