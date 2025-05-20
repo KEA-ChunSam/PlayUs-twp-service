b7d5594c3f2d3977460520e5a3874cfb9e87c562
han16935 <han1693516@gmail.com>
2025-05-20 20:47:12 +0900

    feat : PartyJoinRequestStatus 와 파티에 따라 PartyJoinDocument 가져오는 findByPartyIdAndStatus() 작성

--- src/main/java/com/playus/twpservice/domain/party/repository/read/PartyJoinReadOnlyRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/read/PartyJoinReadOnlyRepository.java
@@ -2,12 +2,17 @@ package com.playus.twpservice.domain.party.repository.read;
 
 import com.playus.twpservice.domain.common.data.BaseMongoRepository;
 import com.playus.twpservice.domain.party.document.PartyJoinDocument;
+import com.playus.twpservice.domain.party.enums.PartyJoinRequestStatus;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PartyJoinReadOnlyRepository extends BaseMongoRepository<PartyJoinDocument, Long> {
 
     @Query(value = "{'userId': ?0, 'partyId': ?1, 'deletedAt' : null}")
     Optional<PartyJoinDocument> findByUserIdAndPartyId(Long userId, Long partyId);
+
+    @Query(value = "{'partyId': ?0, 'partyJoinRequestStatus' : ?1, 'deletedAt' : null}")
+    List<PartyJoinDocument> findByPartyIdAndStatus(Long partyId, PartyJoinRequestStatus partyJoinRequestStatus);
 }
