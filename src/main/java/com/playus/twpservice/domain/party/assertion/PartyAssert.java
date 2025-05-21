d031e73f4569c11db5184be5299cc65b9fd85c5a
han16935 <han1693516@gmail.com>
2025-05-21 11:10:09 +0900

    feat : isAppliableParty() 에 나이, 성별 관련 validation 로직 추가

--- src/main/java/com/playus/twpservice/domain/party/assertion/PartyAssert.java
+++ src/main/java/com/playus/twpservice/domain/party/assertion/PartyAssert.java
@@ -1,8 +1,11 @@
 package com.playus.twpservice.domain.party.assertion;
 
+import com.playus.twpservice.domain.common.security.Gender;
 import com.playus.twpservice.domain.party.entity.Party;
+import com.playus.twpservice.domain.party.enums.PartyAgeGroup;
 import org.springframework.util.Assert;
 
+import java.util.List;
 import java.util.Objects;
 
 import static com.playus.twpservice.domain.party.exception.entity.PartyException.*;
@@ -24,9 +27,19 @@ public class PartyAssert extends Assert {
     }
 
     // 409
-    public static void isAppliableParty(Party party) {
+    public static void isAppliableParty(Party party, List<PartyAgeGroup> partyAgeGroupList, Gender userGender, PartyAgeGroup userAgeGroup) {
         if (party.getCurrentParticipants() >= party.getMaximumParticipants()) {
             throw new ExceedPartyParticipantsException("직관팟 정원이 초과되었습니다!");
+        }
+
+        // 400
+        if (userGender != Gender.UNDEFINED && !party.getPartyGender().equals(userGender.toPartyGender())) {
+            throw new NotAllowedPartyConditionException("직관팟 성별이 맞지 않습니다!");
+        }
+
+        // 400
+        if (!partyAgeGroupList.contains(userAgeGroup)) {
+            throw new NotAllowedPartyConditionException("직관팟 나이가 맞지 않습니다!");
         }
     }
 }
