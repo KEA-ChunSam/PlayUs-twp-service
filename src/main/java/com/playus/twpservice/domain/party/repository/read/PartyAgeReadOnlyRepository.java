0fdf70976dfa1ec846abc414ac4c6fa937a4ca72
han16935 <han1693516@gmail.com>
2025-05-21 11:09:43 +0900

    feat : 특정 직관팟의 나이 가져오기 위한 findByPartyId() 작성

--- src/main/java/com/playus/twpservice/domain/party/repository/read/PartyAgeReadOnlyRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/read/PartyAgeReadOnlyRepository.java
@@ -2,6 +2,11 @@ package com.playus.twpservice.domain.party.repository.read;
 
 import com.playus.twpservice.domain.common.data.BaseMongoRepository;
 import com.playus.twpservice.domain.party.document.PartyAgeDocument;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface PartyAgeReadOnlyRepository extends BaseMongoRepository <PartyAgeDocument, Long> {
+    @Query(value = "{'party_id' : ?0, 'deletedAt' : null}")
+    List<PartyAgeDocument> findByPartyId(Long partyId);
 }
