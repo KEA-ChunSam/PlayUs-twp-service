9c67ad5faf5a2f23c5bcbfb5cf587e0f26e4beb2
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 03:07:57 +0900

    feat: `Kafka` 설정 추가

---
+++ src/main/java/com/playus/twpservice/global/config/kafka/KafkaBeanSuffix.java
@@ -0,0 +1,8 @@
+package com.playus.twpservice.global.config.kafka;
+
+public abstract class KafkaBeanSuffix {
+
+  public static final String PRODUCER_FACTORY_SUFFIX = "ProducerFactory";
+  public static final String NEW_TOPIC_SUFFIX = "Topic";
+  public static final String KAFKA_TEMPLATE_SUFFIX = "KafkaTemplate";
+}
