45251b8746a8134460ec0e478c4abcde09d1efd6
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 03:12:03 +0900

    refactor: 채팅방 `Repository` 변경
    - `BaseMongoRepository` -> `JpaRepository`

--- src/main/java/com/playus/twpservice/domain/chat/repository/ChatRoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.playus.twpservice.domain.chat.repository;
-
-import com.playus.twpservice.domain.chat.entity.ChatRoom;
-import com.playus.twpservice.domain.common.data.BaseMongoRepository;
-
-public interface ChatRoomRepository extends BaseMongoRepository<ChatRoom, String> {
-
-}
