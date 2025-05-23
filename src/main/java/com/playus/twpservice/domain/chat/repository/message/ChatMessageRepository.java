03e89dea3c60cb97d3d74bb3e58d78b902035cbf
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 03:17:30 +0900

    refactor: 메시지 조회 로직 개선
    - `repository.message`로 패키지 이동
    - `soft delete` 삭제
    - `findAllByChatRoomIdAndLastReadAtAfterOrderByLastReadAtDesc` 메서드 추가
    - `lastReadAt` 기준 범위 조회 메서드 추가
    - `lastReadAt` 이후 메시지 개수 카운트 메서드 추가

---
+++ src/main/java/com/playus/twpservice/domain/chat/repository/message/ChatMessageRepository.java
@@ -0,0 +1,31 @@
+package com.playus.twpservice.domain.chat.repository.message;
+
+import com.playus.twpservice.domain.chat.document.ChatMessage;
+import com.playus.twpservice.domain.common.data.BaseMongoRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ChatMessageRepository extends BaseMongoRepository<ChatMessage, String> {
+
+    @Query("{ 'chatpart_id' : {$in :  ?0} }")
+    void deleteAllByChatParticipantIds(List<Long> chatParticipantIds);
+
+    Slice<ChatMessage> findAllByChatRoomIdAndLastReadAtAfterOrderByLastReadAtDesc(Long chatRoomId, LocalDateTime joinedAt, Pageable pageable);
+
+    @Query("""
+            {
+                'chat_room_id': ?0,
+                'last_read_at': {
+                    $gt: ?1,
+                    $lt: ?2
+                }
+            }
+            """)
+    Slice<ChatMessage> findAllByRoomIdAndLastReadAtAfterAndLastReadAtBeforeOrderByLastReadAtDesc(Long chatRoomId, LocalDateTime joinedAt, LocalDateTime lastMessageTimeStamp, Pageable pageable);
+
+    Integer countAllByChatRoomIdAndLastReadAtAfterOrderByLastReadAtDesc(Long chatRoomId, LocalDateTime lastReadAt);
+}
