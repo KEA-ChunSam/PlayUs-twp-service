4c0563f6901c6aeab25f378bd53ac352ebdf29b0
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 04:37:21 +0900

    refactor: 내부 통신 API path 변경

--- src/main/java/com/playus/twpservice/domain/party/feign/client/UserFeignClient.java
+++ src/main/java/com/playus/twpservice/domain/party/feign/client/UserFeignClient.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "userFeignClient", url = "${feign.user.url}", path = "/user", fallback = UserFeignFallback.class)
+@FeignClient(name = "userFeignClient", url = "${feign.user.url}", path = "/user/api", fallback = UserFeignFallback.class)
 @CircuitBreaker(name = "circuit")
 public interface UserFeignClient {
 
