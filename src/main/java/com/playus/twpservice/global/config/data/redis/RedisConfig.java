6e43a4679819f537ba0b9e120bfa738abbeb6935
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 03:05:53 +0900

    feat: 채팅 기능을 위한 `RedisTemplate` 추가

--- src/main/java/com/playus/twpservice/global/config/data/redis/RedisConfig.java
+++ src/main/java/com/playus/twpservice/global/config/data/redis/RedisConfig.java
@@ -1,10 +1,20 @@
 package com.playus.twpservice.global.config.data.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.playus.twpservice.domain.chat.dto.request.ChattingMessage;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class RedisConfig {
@@ -18,5 +28,35 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+
+
+    @Bean
+    @Qualifier("chatRedisTemplate")
+    public RedisTemplate<String, ChattingMessage> chatRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, ChattingMessage> redisTemplate = new RedisTemplate<>();
+
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+        Jackson2JsonRedisSerializer<ChattingMessage> serializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, ChattingMessage.class);
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+
+        return redisTemplate;
+    }
+
+    @Bean
+    @Qualifier("chatRoomRedisTemplate")
+    public RedisTemplate<String, Object> chatRoomRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
     }
 }
