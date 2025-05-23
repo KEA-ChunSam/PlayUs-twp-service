b5598cfcf8bbaaa6ffec75330ac90345c4c15f49
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 03:24:35 +0900

    feat: 채팅 참여 기능 구현

---
+++ src/main/java/com/playus/twpservice/domain/chat/service/ChatParticipantService.java
@@ -0,0 +1,87 @@
+package com.playus.twpservice.domain.chat.service;
+
+import com.playus.twpservice.domain.chat.dto.request.ChattingMessage;
+import com.playus.twpservice.domain.chat.dto.response.ReadMessageRange;
+import com.playus.twpservice.domain.chat.entity.ChatParticipant;
+import com.playus.twpservice.domain.chat.entity.ChatRoom;
+import com.playus.twpservice.domain.chat.entity.enums.MessageType;
+import com.playus.twpservice.domain.chat.exception.common.SubscribeException;
+import com.playus.twpservice.domain.chat.exception.entity.ChatParticipantException;
+import com.playus.twpservice.domain.chat.repository.read.ChatParticipantReadOnlyRepository;
+import com.playus.twpservice.domain.chat.repository.write.ChatParticipantRepository;
+import com.playus.twpservice.domain.chat.repository.message.custom.ChatMessageRepositoryCustom;
+import com.playus.twpservice.domain.party.feign.client.UserFeignClient;
+import com.playus.twpservice.domain.chat.kafka.KafkaChatPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatParticipantService {
+
+    private final ChatParticipantRepository chatParticipantRepository;
+    private final ChatParticipantReadOnlyRepository chatParticipantReadOnlyRepository;
+    private final ChatMessageRepositoryCustom chatMessageRepository;
+    private final ChatRedisService chatRedisService;
+    private final KafkaChatPublisher kafkaChatPublisher;
+    private final UserFeignClient userFeignClient;
+
+    public void save(ChatParticipant chatParticipant) {
+        chatParticipantRepository.save(chatParticipant);
+    }
+
+    public ChatParticipant find(ChatRoom chatRoom, Long userId) {
+        return chatParticipantRepository.findByChatRoomAndUserId(chatRoom, userId)
+                .orElseThrow(() -> new ChatParticipantException.NotFoundException("존재하지 않는 채팅 참여자 입니다."));
+    }
+
+    public ChatParticipant find(long roomId, long memberId) {
+        return chatParticipantRepository.findByChatRoomIdAndUserId(roomId, memberId)
+                .orElseThrow(() -> new ChatParticipantException.NotFoundException("존재하지 않는 채팅 참여자 입니다."));
+    }
+
+    public boolean checkSubscription(ChatRoom chatRoom, Long userId) {
+        Optional<ChatParticipant> optionalParticipant = chatParticipantRepository.findByChatRoomAndUserId(chatRoom, userId);
+
+        if (optionalParticipant.isPresent()) {
+            ChatParticipant chattingParticipant = optionalParticipant.get();
+
+            checkDuplicateSubscription(chatRoom.getId(), userId);
+
+            Pair<String, String> pair = chatMessageRepository.updateUnreadCount(chatRoom.getId(), chattingParticipant.getLastReadAt(), userId);
+
+            String nickname = userFeignClient.getUserInfo(userId).nickname();
+
+            reEnterEvent(chatRoom.getId(), userId, nickname, ReadMessageRange.from(pair));
+
+            chattingParticipant.reSubscribe();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public void delete(ChatParticipant chattingParticipant) {
+        chatParticipantRepository.delete(chattingParticipant);
+    }
+
+    public long getParticipantCount(Long charRoomId) {
+        return chatParticipantReadOnlyRepository.countByChatRoomId(charRoomId);
+    }
+
+    private void checkDuplicateSubscription(long roomId, long memberId) {
+        if (chatRedisService.isActive(roomId, memberId)) {
+            throw new SubscribeException.DuplicateSubscribeException("같은 채팅방을 중복으로 구독했습니다.");
+        }
+    }
+
+    private void reEnterEvent(long chatRoomId, long senderId, String senderName, ReadMessageRange range) {
+        ChattingMessage chattingMessage = ChattingMessage.of(chatRoomId, senderId, senderName, range, MessageType.READ);
+
+        kafkaChatPublisher.publish(chattingMessage);
+    }
+}
