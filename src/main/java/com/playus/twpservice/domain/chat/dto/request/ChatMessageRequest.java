c2877685fa3526563ab41007e0e2dc13be49211f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 02:55:04 +0900

    feat: 채팅 기능 구현을 위한 요청 `DTO` 생성

---
+++ src/main/java/com/playus/twpservice/domain/chat/dto/request/ChatMessageRequest.java
@@ -0,0 +1,8 @@
+package com.playus.twpservice.domain.chat.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ChatMessageRequest(
+        @NotBlank String message
+) {
+}
