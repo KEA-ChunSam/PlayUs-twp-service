d9c258e92b85eb64bacc003fd7fbcee35f894024
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 03:10:04 +0900

    feat: 채팅 기능을 위한 `Redis` 로직 구현

---
+++ src/main/java/com/playus/twpservice/domain/chat/service/ChatRedisService.java
@@ -0,0 +1,58 @@
+package com.playus.twpservice.domain.chat.service;
+
+import com.playus.twpservice.domain.chat.exception.common.SubscribeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRedisService {
+    public static final String REDIS_CHAT_KEY_PREFIX = "ROOM:";
+
+    private final RedisTemplate<String, Object> chatRoomRedisTemplate;
+
+    public void saveSubscribeMember(long roomId, long senderId, String profilePicture) {
+        String key = getKey(roomId);
+
+        chatRoomRedisTemplate.opsForHash().put(key, String.valueOf(senderId), profilePicture);
+    }
+
+    public boolean isActive(long roomId, long senderId) {
+        String key = getKey(roomId);
+
+        return Boolean.TRUE.equals(chatRoomRedisTemplate.opsForHash().hasKey(key, String.valueOf(senderId)));
+    }
+
+    public void removeSubscribeMember(long roomId, long senderId) {
+        String key = getKey(roomId);
+
+        chatRoomRedisTemplate.opsForHash().delete(key, String.valueOf(senderId));
+    }
+
+    public void checkSubscriptionStatus(long roomId, long senderId) {
+        if (!isActive(roomId, senderId)) {
+            throw new SubscribeException.UnSubscriptionException("채팅방을 구독하지 않은 참여자 입니다.");
+        }
+    }
+
+    public long getSubscriberCount(long roomId) {
+        String key = getKey(roomId);
+
+        return chatRoomRedisTemplate.opsForHash().size(key);
+    }
+
+    public String getProfileImageUrl(long roomId, long senderId) {
+        String key = getKey(roomId);
+
+        return Optional.ofNullable(chatRoomRedisTemplate.opsForHash().get(key, String.valueOf(senderId)))
+                .map(Object::toString)
+                .orElse(null);
+    }
+
+    private String getKey(long roomId) {
+        return REDIS_CHAT_KEY_PREFIX + roomId;
+    }
+}
