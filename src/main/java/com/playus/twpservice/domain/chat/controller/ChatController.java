0da4a6c81cf91bb4f8ee4904f979d69c6fc8c6d5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-24 20:00:11 +0900

    refactor: `Swagger` 문서화

--- src/main/java/com/playus/twpservice/domain/chat/controller/ChatController.java
+++ src/main/java/com/playus/twpservice/domain/chat/controller/ChatController.java
@@ -5,8 +5,8 @@ import com.playus.twpservice.domain.chat.dto.response.ChatResponse;
 import com.playus.twpservice.domain.chat.dto.response.ChatUserInfoResponse;
 import com.playus.twpservice.domain.chat.service.ChatRoomService;
 import com.playus.twpservice.domain.chat.service.ChattingService;
+import com.playus.twpservice.domain.chat.specification.ChatControllerSpecification;
 import com.playus.twpservice.domain.common.security.CustomOAuth2User;
-import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,41 +21,42 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
-public class ChatController {
+public class ChatController implements ChatControllerSpecification {
 
     private final ChattingService chattingService;
     private final ChatRoomService chattingRoomService;
 
+    @Override
     @GetMapping("/chat/{roomId}")
-    @Operation(summary = "채팅방 재입장시 이전 메시지를 조회하기 위한 API입니다.")
-    public ResponseEntity<ChatResponse> getChattingMessages(@PathVariable Long roomId,
-                                                            @AuthenticationPrincipal CustomOAuth2User principal,
-                                                            @RequestParam int pageNumber,
-                                                            @RequestParam int pageSize,
-                                                            @RequestParam(required = false) LocalDateTime lastMessageTimeStamp) {
+    public ResponseEntity<ChatResponse> getChattingMessages(@AuthenticationPrincipal CustomOAuth2User principal,
+                                                          @PathVariable Long roomId,
+                                                          @RequestParam int pageNumber,
+                                                          @RequestParam int pageSize,
+                                                          @RequestParam(required = false) LocalDateTime lastMessageTimeStamp) {
         ChatResponse response = chattingService.getMessage(roomId, principal.getId(), pageNumber, pageSize, lastMessageTimeStamp);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Override
     @GetMapping("/chat/count/{roomId}")
-    @Operation(summary = "채팅방의 총 참여자 수와 참여 멤버를 조회하기 위한 API입니다.")
     public ResponseEntity<ChatUserInfoResponse> getChattingMessageCount(@AuthenticationPrincipal CustomOAuth2User principal,
-                                                                     @PathVariable Long roomId) {
+                                                                      @PathVariable Long roomId) {
         ChatUserInfoResponse response = chattingRoomService.getCount(principal.getId(), roomId);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Override
     @DeleteMapping("/chat/{roomId}")
-    @Operation(summary = "채팅방을 퇴장하는 API입니다. 퇴장시 STOMP UNSUBSCRIBE를 꼭 해주세요")
-    public ResponseEntity<Void> exitChattingRoom(@PathVariable Long roomId,
-                                              @AuthenticationPrincipal CustomOAuth2User principal) {
+    public ResponseEntity<Void> exitChattingRoom(@AuthenticationPrincipal CustomOAuth2User principal,
+                                               @PathVariable Long roomId) {
         chattingRoomService.exitChatRoom(roomId, principal.getId());
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Override
     @MessageMapping("/chat/message")
     public void message(@Valid ChatMessageRequest request, SimpMessageHeaderAccessor headerAccessor) {
         chattingService.chat(request, headerAccessor);
