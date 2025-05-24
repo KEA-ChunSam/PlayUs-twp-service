2dbf41ac630cbbe3e9ae85a0ea64d59ee72b43b4
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-25 01:58:26 +0900

    feat: `postProcessBeanFactory` 메서드 구현

--- src/main/java/com/playus/twpservice/global/config/kafka/KafkaBeanRegistrar.java
+++ src/main/java/com/playus/twpservice/global/config/kafka/KafkaBeanRegistrar.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -26,75 +27,79 @@ import static com.playus.twpservice.global.config.kafka.KafkaBeanSuffix.*;
 @Component
 public class KafkaBeanRegistrar implements BeanDefinitionRegistryPostProcessor, EnvironmentAware {
 
-  private Environment environment;
+    private Environment environment;
 
-  @Override
-  public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
-      throws BeansException {
-    Map<String, String> topics = Binder.get(environment)
-        .bind("spring.kafka.topics", Bindable.mapOf(String.class, String.class))
-        .orElse(Collections.emptyMap());
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
+            throws BeansException {
+        Map<String, String> topics = Binder.get(environment)
+                .bind("spring.kafka.topics", Bindable.mapOf(String.class, String.class))
+                .orElse(Collections.emptyMap());
 
-    for (String topic : topics.values()) {
-      this.registerProducerFactoryAndKafkaTemplate(topic, registry);
-      this.registerNewTopic(topic, registry);
+        for (String topic : topics.values()) {
+            this.registerProducerFactoryAndKafkaTemplate(topic, registry);
+            this.registerNewTopic(topic, registry);
+        }
     }
-  }
 
-  private void registerKafkaTemplate(String topic, String producerFactoryBeanName, BeanDefinitionRegistry registry) {
-    String kafkaTemplateBeanName = KafkaBeanUtils.getBeanName(topic, KAFKA_TEMPLATE_SUFFIX);
-    AbstractBeanDefinition kafkaTemplateBeanDefinition =
-        BeanDefinitionBuilder.genericBeanDefinition(KafkaTemplate.class)
-            .addConstructorArgReference(producerFactoryBeanName)
-            .getBeanDefinition();
+    private void registerKafkaTemplate(String topic, String producerFactoryBeanName, BeanDefinitionRegistry registry) {
+        String kafkaTemplateBeanName = KafkaBeanUtils.getBeanName(topic, KAFKA_TEMPLATE_SUFFIX);
+        AbstractBeanDefinition kafkaTemplateBeanDefinition =
+                BeanDefinitionBuilder.genericBeanDefinition(KafkaTemplate.class)
+                        .addConstructorArgReference(producerFactoryBeanName)
+                        .getBeanDefinition();
 
-    registry.registerBeanDefinition(kafkaTemplateBeanName, kafkaTemplateBeanDefinition);
-  }
+        registry.registerBeanDefinition(kafkaTemplateBeanName, kafkaTemplateBeanDefinition);
+    }
 
-  private void registerNewTopic(String topic, BeanDefinitionRegistry registry) {
-    short partitionCount = Short.valueOf(this.environment.getProperty("spring.kafka.partition-count"));
-    short replicationFactor = Short.valueOf(this.environment.getProperty("spring.kafka.replication-factor"));
+    private void registerNewTopic(String topic, BeanDefinitionRegistry registry) {
+        short partitionCount = Short.valueOf(this.environment.getProperty("spring.kafka.partition-count"));
+        short replicationFactor = Short.valueOf(this.environment.getProperty("spring.kafka.replication-factor"));
 
-    String topicBeanName = KafkaBeanUtils.getBeanName(topic, NEW_TOPIC_SUFFIX);
-    AbstractBeanDefinition newTopicBeanDefinition =
-        BeanDefinitionBuilder.genericBeanDefinition(NewTopic.class)
-        .addConstructorArgValue(topic)
-        .addConstructorArgValue(partitionCount)
-        .addConstructorArgValue(replicationFactor)
-        .getBeanDefinition();
+        String topicBeanName = KafkaBeanUtils.getBeanName(topic, NEW_TOPIC_SUFFIX);
+        AbstractBeanDefinition newTopicBeanDefinition =
+                BeanDefinitionBuilder.genericBeanDefinition(NewTopic.class)
+                        .addConstructorArgValue(topic)
+                        .addConstructorArgValue(partitionCount)
+                        .addConstructorArgValue(replicationFactor)
+                        .getBeanDefinition();
 
-    registry.registerBeanDefinition(topicBeanName, newTopicBeanDefinition);
-  }
+        registry.registerBeanDefinition(topicBeanName, newTopicBeanDefinition);
+    }
 
-  private void registerProducerFactoryAndKafkaTemplate(String topic, BeanDefinitionRegistry registry) {
-    String producerBeanName = KafkaBeanUtils.getBeanName(topic, PRODUCER_FACTORY_SUFFIX);
-    AbstractBeanDefinition producerBeanDefinition =
-        BeanDefinitionBuilder.genericBeanDefinition(DefaultKafkaProducerFactory.class)
-            .addConstructorArgValue(this.getProducerOptions())
-            .getBeanDefinition();
+    private void registerProducerFactoryAndKafkaTemplate(String topic, BeanDefinitionRegistry registry) {
+        String producerBeanName = KafkaBeanUtils.getBeanName(topic, PRODUCER_FACTORY_SUFFIX);
+        AbstractBeanDefinition producerBeanDefinition =
+                BeanDefinitionBuilder.genericBeanDefinition(DefaultKafkaProducerFactory.class)
+                        .addConstructorArgValue(this.getProducerOptions())
+                        .getBeanDefinition();
 
-    registry.registerBeanDefinition(producerBeanName, producerBeanDefinition);
+        registry.registerBeanDefinition(producerBeanName, producerBeanDefinition);
 
-    this.registerKafkaTemplate(topic, producerBeanName, registry);
-  }
+        this.registerKafkaTemplate(topic, producerBeanName, registry);
+    }
 
-  private Map<String, Object> getProducerOptions() {
-    String bootstrapServers = this.environment.getProperty("spring.kafka.bootstrap-servers");
+    private Map<String, Object> getProducerOptions() {
+        String bootstrapServers = this.environment.getProperty("spring.kafka.bootstrap-servers");
 
-    Map<String, Object> configs = new HashMap<>();
-    configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
-    configs.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-    configs.put(ProducerConfig.ACKS_CONFIG, "all");
-    configs.put(ProducerConfig.RETRIES_CONFIG, 3);
+        configs.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        configs.put(ProducerConfig.ACKS_CONFIG, "all");
+        configs.put(ProducerConfig.RETRIES_CONFIG, 3);
 
-    return configs;
-  }
+        return configs;
+    }
 
-  @Override
-  public void setEnvironment(Environment environment) {
-    this.environment = environment;
-  }
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+    }
 }
