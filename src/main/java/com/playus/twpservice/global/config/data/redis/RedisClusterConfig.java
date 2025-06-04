b7c5d2f94e9f67fe15b2642334d6ee80de29a10d
han16935 <han1693516@gmail.com>
2025-06-04 17:00:44 +0900

    refactor : RedisClusterConfig 에 chatRedisTemplate, chatRoomRedisTemplate 추가

--- src/main/java/com/playus/twpservice/global/config/data/redis/RedisClusterConfig.java
+++ src/main/java/com/playus/twpservice/global/config/data/redis/RedisClusterConfig.java
@@ -1,5 +1,9 @@
 package com.playus.twpservice.global.config.data.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.playus.twpservice.domain.chat.dto.request.ChattingMessage;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +11,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
 @Profile({"dev", "prod"})  // dev, prod 프로필에서만 사용
@@ -32,5 +42,34 @@ public class RedisClusterConfig {
         clusterConfig.setMaxRedirects(maxRedirects);
 
         return new LettuceConnectionFactory(clusterConfig);
+    }
+
+    @Bean
+    @Qualifier("chatRedisTemplate")
+    public RedisTemplate<String, ChattingMessage> chatRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, ChattingMessage> redisTemplate = new RedisTemplate<>();
+
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+        Jackson2JsonRedisSerializer<ChattingMessage> serializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, ChattingMessage.class);
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+
+        return redisTemplate;
+    }
+
+    @Bean
+    @Qualifier("chatRoomRedisTemplate")
+    public RedisTemplate<String, Object> chatRoomRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
     }
 }
