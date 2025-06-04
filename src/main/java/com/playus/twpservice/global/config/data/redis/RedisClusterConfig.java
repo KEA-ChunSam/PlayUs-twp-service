54b897d21c95460f184c21c37c1cce90763dd916
han16935 <han1693516@gmail.com>
2025-06-04 16:05:33 +0900

    chore : RedisClusterConfig 작성

---
+++ src/main/java/com/playus/twpservice/global/config/data/redis/RedisClusterConfig.java
@@ -0,0 +1,36 @@
+package com.playus.twpservice.global.config.data.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@Profile({"dev", "prod"})  // dev, prod 프로필에서만 사용
+public class RedisClusterConfig {
+
+    @Value("${spring.data.redis.cluster.nodes}")
+    private String redisClusterNodes;
+
+    @Value("${spring.data.redis.cluster.max-redirects}")
+    private int maxRedirects;
+
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
+
+        String[] nodes = redisClusterNodes.split(",");
+        for (String node : nodes) {
+            String[] hostPort = node.trim().split(":");
+            clusterConfig.clusterNode(hostPort[0], Integer.parseInt(hostPort[1]));
+        }
+
+        clusterConfig.setMaxRedirects(maxRedirects);
+
+        return new LettuceConnectionFactory(clusterConfig);
+    }
+}
