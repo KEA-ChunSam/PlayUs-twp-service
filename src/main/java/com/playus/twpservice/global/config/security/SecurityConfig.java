2e1e03fe2b422dab3edab3aeae72b6bce01d8798
zsj <cho010105@naver.com>
2025-06-10 11:09:29 +0900

    [HOTFIX] httpmethod.options 추가

--- src/main/java/com/playus/twpservice/global/config/security/SecurityConfig.java
+++ src/main/java/com/playus/twpservice/global/config/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -59,6 +60,7 @@ public class SecurityConfig {
 
                 // 인증/인가
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(getWhiteList())
                         .permitAll()
                         .anyRequest().authenticated()
